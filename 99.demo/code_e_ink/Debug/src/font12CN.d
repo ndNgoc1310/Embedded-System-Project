313c8a045ac15748bf9cff39d53667fd0bd45f24
Minh Nguyen <158676266+minhgobeo@users.noreply.github.com>
2025-04-18 11:54:10 +0700

    update code to combine

---
+++ 99.demo/code_e_ink/Debug/src/font12CN.d
@@ -0,0 +1,4 @@
+src/font12CN.o: \
+ D:/tai_lieu_quan_trong/Embedded_system/code_e_ink_version_2/code_e_ink/Mylibrary/src/font12CN.c \
+ D:/tai_lieu_quan_trong/Embedded_system/code_e_ink_version_2/code_e_ink/Mylibrary/inc/fonts.h
+D:/tai_lieu_quan_trong/Embedded_system/code_e_ink_version_2/code_e_ink/Mylibrary/inc/fonts.h:
---
+++ 99.demo/code_e_ink/Debug/src/font20.d
@@ -0,0 +1,4 @@
+src/font20.o: \
+ D:/tai_lieu_quan_trong/Embedded_system/code_e_ink_version_2/code_e_ink/Mylibrary/src/font20.c \
+ D:/tai_lieu_quan_trong/Embedded_system/code_e_ink_version_2/code_e_ink/Mylibrary/inc/fonts.h
+D:/tai_lieu_quan_trong/Embedded_system/code_e_ink_version_2/code_e_ink/Mylibrary/inc/fonts.h:
