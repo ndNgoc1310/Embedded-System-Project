71e423367d00da6c765a9ab198075116291a30b0
Nguyễn Duy Ngọc <150082250+ndNgoc1310@users.noreply.github.com>
2025-04-18 12:12:26 +0700

    Revert "E ink display"

--- 99.demo/code_e_ink/Debug/src/font16.d
+++
@@ -1,4 +0,0 @@
-src/font16.o: \
- D:/tai_lieu_quan_trong/Embedded_system/code_e_ink_version_2/code_e_ink/Mylibrary/src/font16.c \
- D:/tai_lieu_quan_trong/Embedded_system/code_e_ink_version_2/code_e_ink/Mylibrary/inc/fonts.h
-D:/tai_lieu_quan_trong/Embedded_system/code_e_ink_version_2/code_e_ink/Mylibrary/inc/fonts.h:
