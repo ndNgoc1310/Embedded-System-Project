a4b0fc81d4a014b9263aa20eb7bc8bbb2eb78758
Nguyễn Duy Ngọc <duyngoc131004@gmail.com>
2025-05-08 00:03:58 +0700

    Final refine

--- 99.demo/code_e_ink/Debug/src/ImageData.d
+++
@@ -1,4 +0,0 @@
-src/ImageData.o: \
- D:/tai_lieu_quan_trong/Embedded_system/code_e_ink_version_2/code_e_ink/Mylibrary/src/ImageData.c \
- D:/tai_lieu_quan_trong/Embedded_system/code_e_ink_version_2/code_e_ink/Mylibrary/inc/ImageData.h
-D:/tai_lieu_quan_trong/Embedded_system/code_e_ink_version_2/code_e_ink/Mylibrary/inc/ImageData.h:
--- 99.demo/code_e_ink/Debug/src/font12CN.d
+++
@@ -1,4 +0,0 @@
-src/font12CN.o: \
- D:/tai_lieu_quan_trong/Embedded_system/code_e_ink_version_2/code_e_ink/Mylibrary/src/font12CN.c \
- D:/tai_lieu_quan_trong/Embedded_system/code_e_ink_version_2/code_e_ink/Mylibrary/inc/fonts.h
-D:/tai_lieu_quan_trong/Embedded_system/code_e_ink_version_2/code_e_ink/Mylibrary/inc/fonts.h:
